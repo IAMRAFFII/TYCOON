17d0f1a4db8fff8711f9a3554f4b43aba181fe61
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2021-12-07 01:04:46 +0530

    Update filters.js

--- plugins/filters.js
+++ plugins/filters.js
@@ -142,7 +142,7 @@ const array = ['Akshan','Anthass','Ayin','Aysheri','Ayye','Ayyo','Bgm','Bye','Da
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
-   await message.client.sendMessage(message.jid, fs.readFileSync('./souravkl11/' + a + '.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted: message.data, ptt: false})
+   await message.client.sendMessage(message.jid, fs.readFileSync('./Tycoon sticker/' + a + '.webp'), MessageType.sticker, { mimetype: Mimetype.webp, quoted: message.data, ptt: false})
 }
 });
 }
