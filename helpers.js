6417b5bae33394ee560facda21dcab8930238840
Its Me Rafi <89293629+Luciferking1@users.noreply.github.com>
2021-12-07 01:31:25 +0530

    Tycoon🎗️

--- helpers.js
+++ helpers.js
@@ -17,13 +17,13 @@
 */
 
 function successfullMessage(msg) {
-    return "✅ *RAGANORK*:  ```" + msg + "```"
+    return "✅ *Tycoon*:  ```" + msg + "```"
 }
 function errorMessage(msg) {
-    return "🛑 *RAGANORK*:  ```" + msg + "```"
+    return "🛑 *Tycoon*:  ```" + msg + "```"
 }
 function infoMessage(msg) {
-    return "⏺️ *RAGANORK*:  ```" + msg + "```"
+    return "⏺️ *Tycoon*:  ```" + msg + "```"
 }
 
 
